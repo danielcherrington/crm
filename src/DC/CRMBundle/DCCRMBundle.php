10d4e91be44cc5e5024e1608af5d800cb63aec59
Daniel Cherrington <daniel.cherrington@gmail.com>
2014-01-11 03:56:42 +0000

    Prototype menu added with font awsome support - overide for crud template generator

--- src/DC/CRMBundle/DCCRMBundle.php
+++ src/DC/CRMBundle/DCCRMBundle.php
@@ -3,7 +3,24 @@
 namespace DC\CRMBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Console\Application;
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
+use Symfony\Component\Filesystem\FileSystem;
 
 class DCCRMBundle extends Bundle
 {
+	/**
+	 * Overide the crud generator so we can control the templates it uses when using the console
+	 * 
+	 * @param  Application $application
+	 * @return [type]
+	 */
+	public function registerCommands(Application $application)
+	{
+	    $crudCommand = $application->get('generate:doctrine:crud');
+	    $generator = new DoctrineCrudGenerator(new FileSystem, __DIR__.'/Resources/skeleton/crud');
+	    $crudCommand->setGenerator($generator);
+
+	    parent::registerCommands($application);
+	}
 }
--- src/DC/CRMBundle/Menu/Builder.php
+++ src/DC/CRMBundle/Menu/Builder.php
@@ -9,22 +9,41 @@ class Builder extends ContainerAware
     public function mainMenu(FactoryInterface $factory, array $options)
     {
        
-
-
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav');
+        $menu->setChildrenAttribute('style', 'display:inline-block;width:600px;');
  
+        $menu->addChild('Home', array('route' => '_default'))
+            ->setAttribute('icon', 'icon-home');
+
         $menu->addChild('Accounts', array('route' => 'account'))
             ->setAttribute('icon', 'icon-list');
  
         $menu->addChild('Contacts', array('route' => 'contact'))
             ->setAttribute('icon', 'icon-group');
+
+        return $menu;
+    }
+
+    public function userMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav pull-right');
  
-        
-
-
-        // ... add more children
-
+        /*
+        You probably want to show user specific information such as the username here. That's possible! Use any of the below methods to do this.
+ 
+        if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
+        $username = $this->container->get('security.context')->getToken()->getUser()->getUsername(); // Get username of the current logged in user
+ 
+        */    
+        $menu->addChild('User', array('label' => 'Hi visitor'))
+            ->setAttribute('dropdown', true)
+            ->setAttribute('icon', 'icon-user');
+ 
+        $menu['User']->addChild('Edit profile', array('route' => '_default'))
+            ->setAttribute('icon', 'icon-edit');
+ 
         return $menu;
     }
 }
